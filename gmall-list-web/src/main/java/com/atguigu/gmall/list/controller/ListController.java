43f54c0e54501bee7ff8527d3c0e3e8021535f65
zhangdd <1165889196@qq.com>
2020-05-12 15:20:31 +0800

    java

--- gmall-list-web/src/main/java/com/atguigu/gmall/list/controller/ListController.java
+++ gmall-list-web/src/main/java/com/atguigu/gmall/list/controller/ListController.java
@@ -2,6 +2,7 @@ package com.atguigu.gmall.list.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.fastjson.JSON;
+import com.atguigu.gmall.bean.SkuLsInfo;
 import com.atguigu.gmall.bean.SkuLsParams;
 import com.atguigu.gmall.bean.SkuLsResult;
 import com.atguigu.gmall.service.ListService;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
 
@@ -16,10 +18,14 @@ import java.util.List;
 public class ListController {
     @Reference
     private ListService listService;
+
     @RequestMapping("list.html")
-    @ResponseBody
-    public String list(SkuLsParams skuLsParams) throws IOException {
-        SkuLsResult search = listService.search(skuLsParams);
-        return JSON.toJSONString(search);
+    public String list(SkuLsParams skuLsParams, HttpServletRequest request) throws IOException {
+        //SkuLsResult search = listService.search(skuLsParams);
+        //return JSON.toJSONString(search);
+        SkuLsResult skuLsResult = listService.search(skuLsParams);
+        List<SkuLsInfo> skuLsInfoList = skuLsResult.getSkuLsInfoList();
+        request.setAttribute("skuLsInfoList",skuLsInfoList);
+        return "list";
     }
 }
